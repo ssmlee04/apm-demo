eb34f2da71d0dfd6587ad176785c5f922d5fd787
Shih-Min Lee <ssmlee04@gmail.com>
2022-04-07 13:31:05 -0400

    add dd-trace

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const tracer = require('dd-trace')
 const app = express()
 const port = 3000
 
+tracer.init({
+  enabled: true,
+  profiling: true,
+  service: 'apm-demo',
+});
+
+const some_function = () => {
+  const parentSpan = tracer.scope().active()
+  for (var i = 0; i < 100; i++) {
+    const span = tracer.startSpan(i, { childOf: parentSpan })
+    span.finish()
+  }
+}
+
 app.get('/', (req, res) => {
+  some_function()
   res.send('Hello World!')
 })
 
